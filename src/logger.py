abaa51b09cac2f37cd021f4efa758ac67d681250
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2024-11-11 14:18:37 +0330

    add cache and sync logic

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,23 @@
 import inspect
+import json
 import os
 import uuid
 from datetime import datetime, timezone
 
+from bson import ObjectId
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        return super().default(obj)
+
+
 class OmniLog:
+    # TODO: Different DBs
     def __init__(
         self,
         service_name,
@@ -15,6 +25,7 @@ class OmniLog:
         task_id=None,
         db_name="omni_logs",
         collection="logs",
+        fallback_dir="cache_logs",
     ):
         load_dotenv()
         self.service_name = service_name
@@ -23,6 +34,10 @@ class OmniLog:
         client = AsyncIOMotorClient(mongoURI)
         db = client[db_name]
         self.collection = db[collection]
+        self.fallback_dir = fallback_dir
+
+        if not os.path.exists(self.fallback_dir):
+            os.makedirs(self.fallback_dir)
 
     async def started(self, message="", context=None):
         await self._log_with_status("Started", message, context)
@@ -37,7 +52,8 @@ class OmniLog:
         await self.log(log_level="WARNING", message=message, context=context)
 
     async def log(self, log_level="INFO", message="", context=None):
-        frame = inspect.currentframe().f_back()
+        frame = inspect.currentframe().f_back
+
         caller_info = {
             "function_name": frame.f_code.co_name,
             "file_name": frame.f_code.co_filename,
@@ -56,8 +72,20 @@ class OmniLog:
 
         try:
             await self.collection.insert_one(data)
-        except Exception as e:
-            print(f"Failed to log on MongoDB: {e}")
+        except:
+            if self.fallback_dir:
+                self._log_to_local_fallback(data)
+            else:
+                print("Failed to log on MongoDB")
+
+    def _log_to_local_fallback(self, log_data):
+        file_name = os.path.join(self.fallback_dir, f"log_{uuid.uuid4()}.json")
+        try:
+            with open(file_name, "w") as f:
+                json.dump(log_data, f, cls=CustomJSONEncoder)
+            print(f"Saved log to local fallback: {file_name}")
+        except IOError as e:
+            print(f"Failed to write to local fallback file: {e}")
 
     async def _log_with_status(self, status, message, context):
         await self.log(message=f"{message} {status}", context=context)
